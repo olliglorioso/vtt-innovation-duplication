805ecba98680e4cfc9ef27b1de0f168f0bb3364e
Olli Glorioso <olli.glorioso8@gmail.com>
2025-05-31 14:15:34 +0300

    Add data extractor

---
+++ data_extractor.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+class Extractor():
+    def __init__(self):
+        pass
+        
+    def create_text_to_compare(self, df):
+        text_to_compare = df["source id"] + " - " + df["source description"].fillna("") + " | Developed by " + df["target english_id"].fillna("")
+        return text_to_compare    
+    
+    def extract(self):
+        # Extract the CSV files
+        vtt_domain_df = pd.read_csv("./data/dataframes/df_relationships_vtt_domain.csv")
+        comp_domain_df = pd.read_csv("./data/dataframes/df_relationships_comp_url.csv")
+        
+        vtt_domain_df = vtt_domain_df[vtt_domain_df["relationship type"] == "DEVELOPED_BY"].copy()
+        comp_domain_df = comp_domain_df[comp_domain_df["relationship type"] == "DEVELOPED_BY"].copy()
+        
+        vtt_domain_df["text_to_compare"] = self.create_text_to_compare(vtt_domain_df)
+
+        # vtt_domain_df = vtt_domain_df.drop_duplicates(subset="source description", keep="first")
+
+        comp_domain_df["text_to_compare"] = self.create_text_to_compare(comp_domain_df)
+
+        comp_domain_df = comp_domain_df[comp_domain_df["source type"] != "Organization"]
+        comp_domain_df = comp_domain_df.drop_duplicates(subset="source description", keep="first")
+
+        # compain_domain_df = compain_domain_df.drop_duplicates(subset="source description", keep="first")
+
+        print("Comp domain df length:", len(comp_domain_df), " - VTT domain df length:", len(vtt_domain_df))
+        print("Example of text to compare:", vtt_domain_df.iloc[0]["text_to_compare"])
+                
+        
+        
+        vtt_domain_df["Document number"] = "VTT" + vtt_domain_df["Document number"].astype(str)
+        comp_domain_df["Document number"] = "COMP" + comp_domain_df["Document number"].astype(str)
+
+        
+        df_combined = pd.concat([vtt_domain_df, comp_domain_df], ignore_index=True)
+        df_combined.to_csv("./data/results/df_combined.csv")
+        
+        
